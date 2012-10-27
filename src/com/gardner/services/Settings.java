f8c96d8556a0e6c090357ee2c79796748562d492
Vin Gardner <vcrgardner@gmail.com>
2012-10-27 20:21:36 +0100

    Adds API key.

--- src/com/gardner/services/Settings.java
+++ src/com/gardner/services/Settings.java
@@ -3,7 +3,7 @@ package com.gardner.services;
 public class Settings {
 
 	// PUT YOUR API KEY HERE
-	public static final String API_KEY = "put your key here";
+	public static final String API_KEY = "45e3973e9ddc6545af00461e5b744ef6";
 
 	// Logging Unique Label
 	public static final String LOG_TAG = "PearsonSamDict"
